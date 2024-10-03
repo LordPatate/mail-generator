84ffb19201d027743a9cac36fa111b2d1b7ad2f4
Tom Bricaud <tom.bricaud1@orange.fr>
2024-10-03 14:21:24 +0100

    Main: create file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from datetime import date, time
-from enum import Enum
+from email.generator import BytesGenerator
+from email.message import EmailMessage
+from email.policy import SMTP
 from pathlib import Path
 from typing import NamedTuple
 
@@ -25,3 +27,31 @@ def generate_body_from_template(details: AppointmentDetails) -> str:
         room=details.room,
     )
 
+
+def export_to_file(msg: EmailMessage):
+    with open("mail.eml", mode="wb") as output_fd:
+        (
+            BytesGenerator(output_fd)
+            .flatten(msg)
+        )
+
+
+def main():
+    student_email_address = "mail@example.com"
+    details = AppointmentDetails(
+        date=date(2024, 10, 8),
+        time=time(18),
+    )
+
+    msg = EmailMessage(policy=SMTP)
+    msg["from"] = "mail@example.com"
+    msg["to"] = student_email_address
+    msg["subject"] = "<Mail Subject>"
+    body = generate_body_from_template(details)
+    msg.set_content(body)
+
+    export_to_file(msg)
+
+
+if __name__ == "__main__":
+    main()
